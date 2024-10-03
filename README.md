74eb5c8495699a6e2d39183cdda372f2764e4d42
Ludovic De Matteis <ldematteis@laas.fr>
2024-10-03 14:48:00 +0200

    Add basic project

---
+++ README.md
@@ -0,0 +1,4 @@
+# crocoddyl + CMake example project
+
+This repo is an example on how to use the [crocoddyl](https://github.com/loco-3d/crocoddyl/) optimal control library in C++ using the CMake build system.
+
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <crocoddyl/core/solvers/fddp.hpp>
+#include <crocoddyl/multibody/actions/free-fwddyn.hpp>
+#include <crocoddyl/core/state-base.hpp>
+#include <crocoddyl/core/cost-base.hpp>
+
+int main() 
+{
+  std::cout << "Imports successful" << std::endl;
+  return 0;
+}
